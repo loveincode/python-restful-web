099b08859a4e1c92beaf6702ef8eaa59a21318bf
loveincode <huonefl@163.com>
2017-09-22 17:33:14 +0800

    add host="0.0.0.0", port=80,

--- person.py
+++ person.py
@@ -102,4 +102,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Request Error'}), 400)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80,debug=True)
